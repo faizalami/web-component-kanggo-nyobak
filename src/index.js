1c7ad12919379d4fe81b5bebda5a543f17622d22
Faizal Amiruddin <faizal@wowrack.com>
2021-11-13 22:05:51 +0700

    init tailwind & base layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import './components/ImageFigure'
+import 'tailwindcss/tailwind.css'
+import './layout/AppLayout'
 
-const imageFigure = document.createElement('image-figure')
-imageFigure.setAttribute('src', '/pictures/logo.png')
-imageFigure.setAttribute('alt', 'Test')
-imageFigure.setAttribute('caption', 'Test')
-document.body.appendChild(imageFigure)
+document.body.innerHTML = `
+  <app-layout>
+    Test Layout
+  </app-layout>
+`
--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 const path = require('path')
 
 module.exports = {
@@ -7,11 +8,46 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/favicon.ico',
       filename: 'index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: '**/*',
+          context: path.resolve(__dirname, 'public'),
+          globOptions: {
+            dot: true,
+            gitignore: true,
+            ignore: ['**/*.html', '**/favicon.ico']
+          }
+        }
+      ]
     })
   ]
 }
