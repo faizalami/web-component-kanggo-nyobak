159624e069e4b5740f59291f69d815195e1fac80
Faizal Amiruddin <faizal@wowrack.com>
2021-11-14 07:37:13 +0700

    navigation events

--- src/components/AppNavigation.js
+++ src/components/AppNavigation.js
@@ -1,6 +1,34 @@
 class AppNavigation extends HTMLElement {
   connectedCallback () {
+    this._showUserMenu = false
+    this._showMobileMenu = false
     this.render()
+  }
+
+  _initEvents () {
+    this.querySelector('#user-menu-button').addEventListener('click', () => {
+      this._showUserMenu = !this._showUserMenu
+      if (this._showUserMenu) {
+        this.querySelector('[role=menu]').classList.remove('transform', 'opacity-0', 'scale-95')
+        this.querySelector('[role=menu]').classList.add('transform', 'opacity-100', 'scale-100')
+      } else {
+        this.querySelector('[role=menu]').classList.remove('transform', 'opacity-100', 'scale-100')
+        this.querySelector('[role=menu]').classList.add('transform', 'opacity-0', 'scale-95')
+      }
+    })
+
+    this.querySelector('[aria-controls=mobile-menu]').addEventListener('click', () => {
+      this._showMobileMenu = !this._showMobileMenu
+      if (this._showMobileMenu) {
+        this.querySelector('#mobile-open-menu-icon').classList.replace('block', 'hidden')
+        this.querySelector('#mobile-close-menu-icon').classList.replace('hidden', 'block')
+        this.querySelector('#mobile-menu').classList.replace('hidden', 'block')
+      } else {
+        this.querySelector('#mobile-open-menu-icon').classList.replace('hidden', 'block')
+        this.querySelector('#mobile-close-menu-icon').classList.replace('block', 'hidden')
+        this.querySelector('#mobile-menu').classList.replace('block', 'hidden')
+      }
+    })
   }
 
   render () {
@@ -36,21 +64,11 @@ class AppNavigation extends HTMLElement {
                   <div>
                     <button type="button" class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                       <span class="sr-only">Open user menu</span>
-                      <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                      <img class="h-8 w-8 rounded-full" src="pictures/profile.jpg" alt="Profile">
                     </button>
                   </div>
     
-                  <!--
-                    Dropdown menu, show/hide based on menu state.
-    
-                    Entering: "transition ease-out duration-100"
-                      From: "transform opacity-0 scale-95"
-                      To: "transform opacity-100 scale-100"
-                    Leaving: "transition ease-in duration-75"
-                      From: "transform opacity-100 scale-100"
-                      To: "transform opacity-0 scale-95"
-                  -->
-                  <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                  <div class="transition ease-out duration-100 transform opacity-0 scale-95 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                     <!-- Active: "bg-gray-100", Not Active: "" -->
                     <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
     
@@ -70,7 +88,7 @@ class AppNavigation extends HTMLElement {
     
                   Menu open: "hidden", Menu closed: "block"
                 -->
-                <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <svg id="mobile-open-menu-icon" class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
                 <!--
@@ -78,7 +96,7 @@ class AppNavigation extends HTMLElement {
     
                   Menu open: "block", Menu closed: "hidden"
                 -->
-                <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <svg id="mobile-close-menu-icon" class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
               </button>
@@ -87,7 +105,7 @@ class AppNavigation extends HTMLElement {
         </div>
     
         <!-- Mobile menu, show/hide based on menu state. -->
-        <div class="md:hidden" id="mobile-menu">
+        <div class="hidden md:hidden" id="mobile-menu">
           <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Products</a>
@@ -97,11 +115,11 @@ class AppNavigation extends HTMLElement {
           <div class="pt-4 pb-3 border-t border-gray-700">
             <div class="flex items-center px-5">
               <div class="flex-shrink-0">
-                <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                <img class="h-10 w-10 rounded-full" src="pictures/profile.jpg" alt="Profile">
               </div>
               <div class="ml-3">
-                <div class="text-base font-medium leading-none text-white">Tom Cook</div>
-                <div class="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
+                <div class="text-base font-medium leading-none text-white">Faizal Amiruddin</div>
+                <div class="text-sm font-medium leading-none text-gray-400">f.a.faizal.amiruddin@gmail.com</div>
               </div>
               <button type="button" class="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                 <span class="sr-only">View notifications</span>
@@ -122,6 +140,7 @@ class AppNavigation extends HTMLElement {
         </div>
       </nav>
    `
+    this._initEvents()
   }
 }
 
